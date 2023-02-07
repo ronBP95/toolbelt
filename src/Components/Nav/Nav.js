79c7623b63aca15c227b678a8443cf3364bb0692
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-06 23:37:48 -0800

    Created a basic nav bar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Logo from '../../images/logos/logo-no-background.png'
 
 export default function Nav() {
   return (
-    <div class="w-fit h-10 bg-gray-200">
-        Nav
+    <div class="flex justify-between items-center w-full h-20 bg-gray-800">
+        <div class="flex">
+            <div class="ml-8">
+                <img src={Logo} alt="Logo" class="w-40"/>
+            </div>
+            <div class="ml-10">
+                <ul class="flex text-white space-x-10">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
+        </div>
     </div>
   )
 }
