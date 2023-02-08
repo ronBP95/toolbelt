11d2f1b778d6622ff63226082b01ffd5332002f8
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-07 22:31:36 -0800

    Added media queries for responsive navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -9,15 +9,20 @@ export default function Nav() {
                 <img src={Logo} alt="Logo" class="w-40"/>
             </div>
             <div class="ml-10">
-                <ul class="flex text-white space-x-10">
+                <ul class="hidden sm:flex text-white space-x-10">
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
             </div>
         </div>
-        <div>
-            <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
+        <div class="flex">
+            <button type="button" class="hidden sm:flex text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
+                <div class="sm:hidden space-y-1.5 mr-8">
+                    <span class="block w-8 h-1 bg-gray-600"></span>
+                    <span class="block w-8 h-1 bg-gray-600"></span>
+                    <span class="block w-8 h-1 bg-gray-600"></span>
+                </div>
         </div>
     </div>
   )
