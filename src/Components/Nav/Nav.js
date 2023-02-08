8c2473d7f0c905e918589f662a796408e31f09a0
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-07 23:00:24 -0800

    Managed to create really cool hovering underline animations using Tailwind

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,21 +3,30 @@ import Logo from '../../images/logos/logo-no-background.png'
 
 export default function Nav() {
   return (
-    <div class="flex justify-between items-center w-full h-20 bg-gray-800">
+    <div class="flex justify-between items-center w-full min-w-fit h-20 bg-gray-800">
         <div class="flex">
             <div class="ml-8">
-                <img src={Logo} alt="Logo" class="w-40"/>
+                <img src={Logo} alt="Logo" class="w-40 max-w-min"/>
             </div>
             <div class="ml-10">
-                <ul class="hidden sm:flex text-white space-x-10">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul class="hidden sm:flex text-white space-x-10 transition duration-200">
+                    <li class="group cursor-pointer hover:text-red-500 transition duration-300">
+                        Home
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-red-500"></span>
+                    </li>
+                    <li class="group cursor-pointer hover:text-red-500 transition duration-300">
+                        About
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-red-500"></span>
+                    </li>
+                    <li class="group cursor-pointer hover:text-red-500 transition duration-300">
+                        Contact
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-red-500"></span>
+                    </li>
                 </ul>
             </div>
         </div>
         <div class="flex">
-            <button type="button" class="hidden sm:flex text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
+            <button type="button" class="hidden sm:flex text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
                 <div class="sm:hidden space-y-1.5 mr-8">
                     <span class="block w-8 h-1 bg-gray-600"></span>
                     <span class="block w-8 h-1 bg-gray-600"></span>
