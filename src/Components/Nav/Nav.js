cac2438cf7f6015e791580065da271755e05e391
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-07 23:43:50 -0800

    A lot of changes to try and make the transition for the button ease in and out

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -26,7 +26,7 @@ export default function Nav() {
             </div>
         </div>
         <div class="flex">
-            <button type="button" class="hidden sm:flex text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Sign In</button>
+            <button type="button" class="hidden sm:flex text-white bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7 ease-out duration-300">Sign In</button>
                 <div class="sm:hidden space-y-1.5 mr-8">
                     <span class="block w-8 h-1 bg-gray-600"></span>
                     <span class="block w-8 h-1 bg-gray-600"></span>
