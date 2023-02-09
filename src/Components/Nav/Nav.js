fb2389cf79f2cb10bee4d8f1fbfebfbd6bb0e495
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-08 23:46:11 -0800

    Prepping space for the Marquee in navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -8,7 +8,7 @@ export default function Nav() {
             <div class="ml-8">
                 <img src={Logo} alt="Logo" class="w-40 max-w-min"/>
             </div>
-            <div class="ml-10">
+            <div class="ml-10 mr-6">
                 <ul class="hidden sm:flex text-white space-x-10 transition duration-200">
                     <li class="group cursor-pointer hover:text-red-500 transition duration-300">
                         Home
@@ -25,8 +25,11 @@ export default function Nav() {
                 </ul>
             </div>
         </div>
-        <div class="flex">
-            <button type="button" class="hidden sm:flex text-white bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7 transition duration-200 hover:scale-110 hover:bg-red-700 ">Sign In</button>
+        <div class="bg-sky-400 w-3/5">
+            Marquee
+        </div>
+        <div class="flex ml-8">
+            <button type="button" class="hidden sm:flex text-white bg-red-600 font-medium rounded-lg text-sm w-24 px-6 py-2.5 text-center mr-7 transition duration-200 hover:scale-110 hover:bg-red-700 ">Sign In</button>
                 <div class="sm:hidden space-y-1.5 mr-8">
                     <span class="block w-8 h-1 bg-gray-600"></span>
                     <span class="block w-8 h-1 bg-gray-600"></span>
