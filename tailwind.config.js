ea3b97620e9228150b1ca2bdbddf570a14ab2fc0
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-02-09 00:15:44 -0800

    Cleared the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  mode: 'jit',
   theme: {
-    extend: {
-      animation: {
-        marquee: 'marquee 25s linear infinite',
-        marquee2: 'marquee2 25s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        marquee2: {
-          '1%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
