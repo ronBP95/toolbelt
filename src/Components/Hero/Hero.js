48740eb337f9b9c2055a17d920117b4090157f10
Ron Magpantay <ronmagpantay9513@gmail.com>
2023-03-01 23:41:46 -0800

    Building out the landing by adding an upload feature. Changed nav from sign in to sign up to prep for adding a login button

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Hero() {
   return (
     <div class='flex justify-center items-center w-full h-screen max-h-fit bg-sky-400'>
-        <div class="text-4xl">Welcome to React Toolbelt!</div>
+        <div>
+          <div class="text-4xl">Upload your file here</div>
+          <div class="flex text-xl">Upload Button<div>+</div></div>
+          <div>Max File Size 50MB (want more?)</div>
+        </div>
     </div>
   )
 }
